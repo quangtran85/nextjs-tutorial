96dc2c5d2bbbe6a98685ad10a865f58950770db1
Nhi Do <nhi.dohuyen@grevo.net>
2023-06-09 18:34:51 +0700

    feature: BECS-00 Search book by title

--- services/inventory.ts
+++ services/inventory.ts
@@ -11,8 +11,13 @@ export interface Paging {
   skip: number;
 }
 
-export const getListBooks = ({limit, skip} : Paging) => {
-  return apiClient.getListBooks(limit, skip).then(result => {
-      return result;
-  })
+export interface Params extends Paging{
+  title?: string;
 }
+
+export const getListBooks = ({ limit, skip, title }: Params) => {
+  return apiClient.getListBooks(limit, skip, title).then((result) => {
+    return result;
+  });
+};
+
